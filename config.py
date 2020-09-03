f4c3cb05e8f9906aea3c5ebdcac8b4e2a71c69c4
Jesse Khorasanee <jessekhorasanee@gmail.com>
2020-09-03 13:48:56 +1200

    Add missing coinmarket key

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 currencylayerkey = os.environ.get("CURRENCY_LAYER_KEY", '*************')
+cm_key = os.environ.get("CM_KEY",'*************')
 coinspot_api_key = os.environ.get("COINSPOT_API_KEY",'*************')
 coinspot_secret = os.environ.get("COINSPOT_SECRET",'*************')
