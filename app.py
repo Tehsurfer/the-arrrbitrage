b44569aec6baa2eb29791b377c2fad79cee21f61
Jesse Khorasanee <jessekhorasanee@gmail.com>
2020-09-05 11:52:46 +1200

    Test threading on heroku

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import threading
+import time
 import RunArbIfDown
 app = Flask(__name__, static_url_path='')
 
@@ -16,7 +17,12 @@ def margins():
 def margins_depth():
     return render_template('margin_table_with_depth.html')
 
+def thread_test():
+    time.sleep(10)
+    print('Thread is printing to console')
+
 
 if __name__ == "__main__":
+    threading.Thread(target=thread_test).start()
     threading.Thread(target=app.run).start()
     threading.Thread(target=RunArbIfDown.start).start()
